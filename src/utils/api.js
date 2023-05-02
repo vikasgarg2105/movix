649402d141bd698a110f43dedbee7d024fb4a310
vikasgarg2105 <vikasgarg2105@gmail.com>
2023-05-02 09:22:46 +0530

    utils for api

---
+++ src/utils/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+
+const headers = {
+  Authorization: "bearer " + TMDB_TOKEN,
+};
+
+export const fetchDataFromApi = async(url, params)=>{
+
+}
